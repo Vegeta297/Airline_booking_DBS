20d70b8efff4675a2532bc4e335ae8ae6eee1e39
Vegeta297 <sarthakraoot@gmail.com>
2025-03-17 23:18:31 +0530

    updated_2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:airline_booking/main.dart';
+import 'package:airline_booking/frontend.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
